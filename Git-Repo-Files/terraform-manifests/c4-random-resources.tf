5c2c91192b62c1117b0e01efd5a0a2ac0c021072
Viswanath-varre <viswanathvarre@outlook.com>
2023-01-31 10:35:23 +0530

    my first files

--- Git-Repo-Files/terraform-manifests/c4-random-resources.tf
+++
@@ -1,7 +0,0 @@
-# Random String Resource
-resource "random_string" "myrandom" {
-  length = 5
-  upper = true 
-  special = false
-  number = true   
-}
--- Git-Repo-Files/terraform-manifests/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-# Generic Variables 
-business_divsion = "visu"
-resource_group_location = "eastus"
-resource_group_name = "webappRG"
